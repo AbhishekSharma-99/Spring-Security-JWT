eb2bc41ffb5f25387880e8636c60a91a05784eb6
AbhishekSharma-99 <abhisheksh3397@gmail.com>
2025-02-07 18:35:02 +0530

    Added User and Role entity class

---
+++ src/main/java/com/abhi/entity/User.java
@@ -0,0 +1,38 @@
+package com.abhi.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // auto-generated primary key
+    private String username;
+    private String password;
+    private String email;
+    private String firstname;
+    private String lastname;
+
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+}
