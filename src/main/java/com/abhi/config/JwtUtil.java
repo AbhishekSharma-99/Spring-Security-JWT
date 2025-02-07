6471c92aa3c698bc8507b95d4c67e11f2a94bb34
AbhishekSharma-99 <abhisheksh3397@gmail.com>
2025-02-08 03:18:25 +0530

    added UserDetailsServiceImpl, UserRepository,

---
+++ src/main/java/com/abhi/config/JwtUtil.java
@@ -0,0 +1,49 @@
+package com.abhi.config;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "9296f4cd3e0efc6f757f21f755576566aa957205b8a555c4aeb238266934d88a";
+    private static final SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
+
+    private Claims extractClaims(String token){
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String username){
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public String generateToken(String username) {
+        return Jwts
+                .builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(key)
+                .compact();
+    }
+}
---
+++ src/main/java/com/abhi/service/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.abhi.service;
+
+import com.abhi.entity.User;
+import com.abhi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(username, username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles()
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                        .collect(Collectors.toSet())
+        );
+    }
+}
